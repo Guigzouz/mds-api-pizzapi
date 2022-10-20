3b22ae457ef187ba279622dd3ee438873019deba
Guillaume Benard <guillaume.benard0@gmail.com>
2022-10-20 16:46:29 +0200

    fin de tp2

--- src/pizzas/dto/create-pizza.dto.ts
+++ src/pizzas/dto/create-pizza.dto.ts
@@ -1,10 +1,16 @@
+import { IsDefined, IsNotEmpty, MaxLength, Min } from "class-validator";
 import { PizzaFlavor } from "../pizza-flavor";
 
 export class CreatePizzaDto {
 
+    @IsNotEmpty()
+    @MaxLength(100)
     name: string;
+    @IsDefined()
     flavor: PizzaFlavor;
     speciality: boolean;
+    @IsDefined()
+    @Min(0)
     price: number;
     end?: Date;
 }
